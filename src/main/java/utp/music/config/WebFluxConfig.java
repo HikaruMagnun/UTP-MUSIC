2094d650d7a75ead4f525edd38c597b62129f926
HikaruMagnun <carlosdiazramos25@hotmail.com>
2025-05-25 18:34:33 -0500

    -- CORS habilitar

--- src/main/java/utp/music/config/WebFluxConfig.java
+++ src/main/java/utp/music/config/WebFluxConfig.java
@@ -1,12 +1,20 @@
 package utp.music.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
-    // This is a marker configuration class that enables WebFlux
-    // You can add custom WebFlux configuration here if needed
+    //Configuarar cors
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false);
+    }
 }
